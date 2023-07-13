fe718dde554febc6254eeaee09eba13b6011f871
Anders <anders.s.olsen@nru.dk>
2023-07-13 14:58:24 +0200

    added license information

--- CopBET_CarhartHarris_2016_data.m
+++ CopBET_CarhartHarris_2016_data.m
@@ -1,4 +1,20 @@
-%%
+%% Function to load CH2016 data structured according to the specifications 
+% in the README file
+% 
+% Copyright (C) 2023 Anders Stevnhoved Olsen & Drummond E-Wen McCulloch
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see http://www.gnu.org/licenses/.
 function [tbl,data,opts] = CopBET_CarhartHarris_2016_data(atlas,ts_ROI2ROI,type)
 if type=='example'
     topfolder = [pwd,'/LSDdata/exampledata/'];
--- CopBET_main_CH2016data.m
+++ CopBET_main_CH2016data.m
@@ -34,6 +34,21 @@
 % Please run the script from the top CopBET directory for the
 % 'CopBET_CarhartHarris_2016_data' function to work properly
 
+% Copyright (C) 2023 Anders Stevnhoved Olsen & Drummond E-Wen McCulloch
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see http://www.gnu.org/licenses/.
+
 clear
 addpath(genpath('/mrdata/np2/p3/entropy/CopBET'))
 
